94f0885defa90b6d9616c13b3cd0ac53754b9c45
TheViruslogBeats <gamew616@gmail.com>
2022-12-22 21:32:23 +0300

    Изменение подключения к базе данных

--- database/connectDb.js
+++ database/connectDb.js
@@ -1,10 +1,22 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("DigitalJustice", "digofjustice", "111222333", {
-  host: "192.168.0.110",
-  port: 49155,
-  dialect: "postgres",
-  logging: false,
-});
+// const sequelize = new Sequelize("DigitalJustice", "digofjustice", "111222333", {
+//   host: "192.168.0.110",
+//   port: 49155,
+//   dialect: "postgres",
+//   logging: false,
+// });
+
+const sequelize = new Sequelize(
+  "DigitalJustice",
+  "DigitalJustice",
+  "confmireamegapassword",
+  {
+    host: "ConfMirea-Postgres",
+    port: 5432,
+    dialect: "postgres",
+    logging: false,
+  }
+);
 
 module.exports = sequelize;
